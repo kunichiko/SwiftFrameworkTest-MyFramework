f931341bcf2c75aa7fd86b9e987f681a9b1432ea
Kunihiko Ohnaka <ohnaka@ripplex.com>
2018-03-08 16:49:25 +0900

    「フレームワークをDebugでビルドし、アプリをReleaseでビルドするとモジュールがロードできなくなってクラッシュする」という問題が再現できた。
    なお、原因は RxSwiftの ObservableType+Extensions.swift の中で、 #if DEBUG によって subscribe関数のシグネチャを切り替えてしまっているところにあった。これは以下のコミットで修正されていて、RxSwift 4.1.2 でリリースされている。
    https://github.com/ReactiveX/RxSwift/commit/2519f45288ec8d26ccffbfd2c15b98a5b6607814#diff-9bcd00dceed4ee32ad6515b12031cdcd

--- MyFramework/Classes/MyFramework.swift
+++ MyFramework/Classes/MyFramework.swift
@@ -10,10 +10,22 @@ import Foundation
 import RxSwift
 
 public class MyFrameworkClass {
+    private let disposeBag = DisposeBag()
+
     public init() {
     }
 
     public func function1() -> Observable<String> {
         return Observable.just("function1")
     }
+
+    public func function2() -> Observable<String> {
+        let v = Variable("function2")
+        v.asObservable().subscribe(onNext: { (str) in
+            NSLog("\(str)")
+        }).disposed(by: disposeBag)
+        return v.asObservable()
+    }
 }
+
+
